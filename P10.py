8c813e251b6b16410150a997e3da5f5a47a6aa9b
relbeyond <beyonder_zeng@163.com>
2026-09-10 19:24:53 +0800

    添加TensorBoard可视化测试集图像

--- P10.py
+++ P10.py
@@ -6,3 +6,12 @@ dataset_transform = torchvision.transforms.Compose([torchvision.transforms.ToTen
 # torchvision.datasets.CIFAR10()下载CIFAR10数据集
 train_set = torchvision.datasets.CIFAR10(root="./dataset", train=True, transform=dataset_transform, download=True)
 test_set = torchvision.datasets.CIFAR10(root="./dataset", train=False, transform=dataset_transform, download=True)
+
+#print(test_set[0])
+
+writer = SummaryWriter("P10")
+for i in range(10):
+    img, label = test_set[i]
+    writer.add_image("test_set", img, i)
+
+writer.close()
